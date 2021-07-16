5499755f0944c089e4759cc99e45802607137016
Piotr Sawicki <psawicki351@gmail.com>
2021-07-16 14:59:22 +0200

    added dynamic pageviews

--- js.js
+++ js.js
@@ -12,24 +12,31 @@ const rangeValue = () => {
 
 const discount = document.querySelector('.check');
 const priceSpan = document.querySelector('.price');
+const pageviews = document.querySelector('.pageviews');
 
 const changePrice = () =>{
     let price;
+    let views;
     switch(rangeValue()){
         case 0:
             price = 8.00;
+            views = '10K';
             break;
         case 25:
             price = 12.00;
+            views = '50K';
             break;
         case 50:
             price = 16.00;
+            views = '100K';
             break;
         case 75:
             price = 24.00;
+            views = '500K';
             break;
         case 100:
             price = 32.00;
+            views = '1M';
             break;
     }
     if(discount.checked){
@@ -40,6 +47,7 @@ const changePrice = () =>{
         priceSpan.classList.remove('promo')
         priceSpan.textContent = `$${price.toFixed(2)}`;
     }
+    pageviews.textContent = `${views} PAGEVIEWS`;
 };
 
 range.addEventListener('input',changePrice);
